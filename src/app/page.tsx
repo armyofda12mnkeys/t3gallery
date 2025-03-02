1e65286640c455f185631f89b9ed25c84f2260e1
ArianH <armyofda12mnkeys@gmail.com>
2025-03-02 18:35:49 -0500

    new things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://hzl0obujh5.ufs.sh/f/cTbLetPhDXg7Mwa6r6btpLozKRTvJxh032Ey4kXOY9n1ZmBs",
@@ -14,10 +15,16 @@ const mockImages = mockUrls.map( (url, index) => ({
 ); 
 
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+  
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
+        {posts.map((post)=>(
+          <div key={post.id}>{post.name}</div>
+        ))}
         {[...mockImages, ...mockImages, ...mockImages].map( (image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
