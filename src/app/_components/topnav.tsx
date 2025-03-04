bb9fa692f4a3116e665db279c36c62cdcc6b8434
ArianH <armyofda12mnkeys@gmail.com>
2025-03-04 14:31:30 -0500

    add uploadthing

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
     ClerkProvider,
     SignInButton,
@@ -6,21 +8,24 @@ import {
     SignedOut,
     UserButton,
   } from '@clerk/nextjs';
+  
+import { UploadButton } from '~/utils/uploadthing';
 
 export function TopNav () {
     return (
       <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
         <div>Nav Gallery</div>
 
-       
-        <SignedOut>
-            <SignInButton />
-            <SignUpButton />
-        </SignedOut>
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
-       
+       <div className='flex flex-row'>
+            <SignedOut>
+                <SignInButton />
+                <SignUpButton />
+            </SignedOut>
+            <SignedIn>
+                <UploadButton endpoint="imageUploader" />
+                <UserButton />
+            </SignedIn>
+        </div>
 
       </nav>
     );
